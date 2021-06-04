10508229294bac94fd518ae8d1f0bc96e3eb55d8
alexandro458 <alejandrovega10.10@gmail.com>
2021-06-04 13:36:43 +0200

    dialogo 2

--- Assets/Scripts/Dialog/PopUpSystem.cs
+++ Assets/Scripts/Dialog/PopUpSystem.cs
@@ -6,45 +6,36 @@ using TMPro;
 public class PopUpSystem : MonoBehaviour
 {
     public GameObject popUpBox;
-    public Animator animator;
+    //public Animator animator;
 
     private Transform _player;
 
-    public TMP_Text popUpText;
+    //public TMP_Text popUpText;
 
-    public void PopUp(string text)
+    /*public void PopUp(string text)
     {
         popUpBox.SetActive(true);
         popUpText.text = text;
         animator.SetTrigger("pop");
-    }
+    }*/
     private void Start()
     {
        popUpBox.SetActive(true);
     }
-    public void FixedUpdate()
-    {
-        if (_player)
-        {
-            popUpBox.SetActive(false);
-        }
-        else
-        {
-            //popUpBox.SetActive(false);
-        }
-    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) _player = collision.gameObject.transform;
+        if (collision.CompareTag("Player")) popUpBox.SetActive(true);
+        
     }
 
     public void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) _player = collision.gameObject.transform;
+        if (collision.CompareTag("Player")) popUpBox.SetActive(true);
     }
 
     public void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) _player = null;
+        if (collision.CompareTag("Player")) popUpBox.SetActive(false);
     }
 }
