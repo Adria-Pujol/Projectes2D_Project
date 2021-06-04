6dc26d08f22fa816933dda6d01c902c41d80970f
alexandro458 <alejandrovega10.10@gmail.com>
2021-06-04 10:58:39 +0200

    Pajaro v1

---
+++ Assets/Scripts/Enemies/Pajaro.cs
@@ -0,0 +1,117 @@
+using System;
+using Unity.Mathematics;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class Pajaro : MonoBehaviour
+    {
+        [Header("Movement")] public float speed;
+
+        [SerializeField] private bool isGround;
+
+        [SerializeField] private bool isWall;
+
+        [SerializeField] private bool isObject;
+
+        [SerializeField] private bool isEnemy;
+
+        private Rigidbody2D _body;
+
+        private GeneralChecker _generalChecker;
+        private EnemyGroundChecker _groundChecker;
+        private bool _isFacingRight = true;
+        private Transform _player;
+
+        public void Awake()
+        {
+            _body = GetComponent<Rigidbody2D>();
+            _generalChecker = transform.Find("GeneralChecker").GetComponent<GeneralChecker>();
+            _groundChecker = transform.Find("GroundChecker").GetComponent<EnemyGroundChecker>();
+        }
+
+        public void FixedUpdate()
+        {
+            //Checking if enemy is colliding to a Wall
+            isWall = _generalChecker.isWall;
+            //Checking if enemy is in Ground
+            isGround = _groundChecker.isGrounded;
+            //Checking if enemy is colliding to an Object
+            isObject = _generalChecker.isObject;
+            //Checking if enemy is colliding to an Enemy
+            isEnemy = _generalChecker.isEnemy;
+
+            if (_player)
+            {
+
+                Vector3 dist2 = new Vector3(_player.position.x - transform.position.x, _player.position.y - transform.position.y, _player.position.z - transform.position.z);
+                //dist2 = Vector3.Normalize(dist2);
+                transform.position = transform.position + (speed * dist2 * Time.deltaTime);
+                /*if (isWall || !isGround || isObject || isEnemy)
+                {
+                    Flip();
+                    var velocity = _body.velocity;
+                    velocity =
+                        _isFacingRight ? new Vector2(speed, velocity.y) : new Vector2(-speed, velocity.y);
+                    _body.velocity = velocity;
+                    _player = null;
+                }
+                else
+                {
+                    Vector2 dist = new Vector2(_player.position.x - transform.position.x, _player.position.y - transform.position.y);
+                    Vector3 dist2 = new Vector3(_player.position.x - transform.position.x, _player.position.y - transform.position.y, _player.position.z - transform.position.z);
+                    dist2 = Vector3.Normalize(dist2);
+                    if (dist.x < 0 && _isFacingRight)
+                    {
+                        Flip();
+                    }
+                    else if (dist.x > 0 && !_isFacingRight)
+                    {
+                        Flip();
+                    } 
+                    transform.position = Vector2.MoveTowards(transform.position,
+                    new Vector2(_player.position.x, transform.position.y), speed * Time.deltaTime * 2); 
+                transform.position = transform.position + (speed * dist2); */
+
+            }
+            else
+            {
+                Patrol();
+            }
+        }
+            
+        
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player")) _player = collision.gameObject.transform;
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player")) _player = null;
+        }
+
+        public void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player")) _player = null;
+        }
+
+        private void Patrol()
+        {
+            if (isWall || !isGround || isObject || isEnemy) Flip();
+
+            var velocity = _body.velocity;
+            velocity = _isFacingRight ? new Vector2(speed, velocity.y) : new Vector2(-speed, velocity.y);
+            _body.velocity = velocity;
+
+        }
+
+        private void Flip()
+        {
+            _isFacingRight = !_isFacingRight;
+            transform.rotation = Quaternion.Euler(0, _isFacingRight ? 0 : 180, 0);
+        }
+    }
+}
