3c97de0351baae971ee236e6c42065c9612c23a1
Adria <adriapujolsierra@gmail.com>
2021-06-11 19:07:32 +0200

    HUD Fixed

--- Assets/Scripts/General/HUD_Controller.cs
+++ Assets/Scripts/General/HUD_Controller.cs
@@ -3,56 +3,164 @@ using System.Collections.Generic;
 using UnityEngine;
 using Player;
 using TMPro;
+using UnityEngine.UI;
 
 public class HUD_Controller : MonoBehaviour
 {
-    private TextMeshProUGUI hp;
-    private TextMeshProUGUI ammunation;
-    private TextMeshProUGUI totalAmmunation;
-    private TextMeshProUGUI bar;
+    
     private TextMeshProUGUI weapon;
     private TextMeshProUGUI collectables;
+    private Image heart1;
+    private Image heart2;
+    private Image heart3;
+    private Image heart4;
+    private Image heart5;
+    private Image heart6;
+    private Image heart7;
+    private Image energy;
+    
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite noHeart;
+    
+    [SerializeField] private Sprite topCapacity;
+    [SerializeField] private Sprite topHalfCapacity;
+    [SerializeField] private Sprite halfCapacity;
+    [SerializeField] private Sprite lowHalfCapacity;
+    [SerializeField] private Sprite lowCapacity;
+
+    
+    
     GameObject player;
 
-    private bool isAmmunationSet = false;
-    private float totalAmmunationNum;
+    private float currentAmmunation;
+    private float health;
 
 
     void Awake()
     {
         player = GameObject.FindWithTag("Player");
-        hp = gameObject.transform.Find("HUD").Find("HP").GetComponent<TextMeshProUGUI>();
+        heart1 = gameObject.transform.Find("HUD").Find("Heart1").GetComponent<Image>();
+        heart2 = gameObject.transform.Find("HUD").Find("Heart2").GetComponent<Image>();
+        heart3 = gameObject.transform.Find("HUD").Find("Heart3").GetComponent<Image>();
+        heart4 = gameObject.transform.Find("HUD").Find("Heart4").GetComponent<Image>();
+        heart5 = gameObject.transform.Find("HUD").Find("Heart5").GetComponent<Image>();
+        heart6 = gameObject.transform.Find("HUD").Find("Heart6").GetComponent<Image>();
+        heart7 = gameObject.transform.Find("HUD").Find("Heart7").GetComponent<Image>();
         weapon = gameObject.transform.Find("HUD").Find("Weapon").GetComponent<TextMeshProUGUI>();
-        ammunation = gameObject.transform.Find("HUD").Find("Ammunation").GetComponent<TextMeshProUGUI>();
-        totalAmmunation = gameObject.transform.Find("HUD").Find("TotalAmmunation").GetComponent<TextMeshProUGUI>();
         collectables = gameObject.transform.Find("HUD").Find("Collected").GetComponent<TextMeshProUGUI>();
-        bar = gameObject.transform.Find("HUD").Find("Bar").GetComponent<TextMeshProUGUI>();
-        isAmmunationSet = false;
+        energy = gameObject.transform.Find("HUD").Find("Energy").GetComponent<Image>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!isAmmunationSet)
+        health = player.GetComponent<PlayerHealth>().health;
+        switch (health)
         {
-            totalAmmunation.text = player.GetComponent<PlayerController>().ammunition.ToString();
-            totalAmmunationNum = player.GetComponent<PlayerController>().ammunition;
-            isAmmunationSet = true;
-        }        
-        hp.text = "Health: " + player.GetComponent<PlayerHealth>().health;
+            case(7):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = fullHeart;
+                heart4.sprite = fullHeart;
+                heart5.sprite = fullHeart;
+                heart6.sprite = fullHeart;
+                heart7.sprite = fullHeart;
+                break;
+            case(6):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = fullHeart;
+                heart4.sprite = fullHeart;
+                heart5.sprite = fullHeart;
+                heart6.sprite = fullHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(5):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = fullHeart;
+                heart4.sprite = fullHeart;
+                heart5.sprite = fullHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(4):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = fullHeart;
+                heart4.sprite = fullHeart;
+                heart5.sprite = noHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(3):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = fullHeart;
+                heart4.sprite = noHeart;
+                heart5.sprite = noHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(2):
+                heart1.sprite = fullHeart;
+                heart2.sprite = fullHeart;
+                heart3.sprite = noHeart;
+                heart4.sprite = noHeart;
+                heart5.sprite = noHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(1):
+                heart1.sprite = fullHeart;
+                heart2.sprite = noHeart;
+                heart3.sprite = noHeart;
+                heart4.sprite = noHeart;
+                heart5.sprite = noHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+            case(0):
+                heart1.sprite = noHeart;
+                heart2.sprite = noHeart;
+                heart3.sprite = noHeart;
+                heart4.sprite = noHeart;
+                heart5.sprite = noHeart;
+                heart6.sprite = noHeart;
+                heart7.sprite = noHeart;
+                break;
+        }
         weapon.text = "Weapon: " + player.GetComponent<PlayerController>().activeWeapon;
         if (player.GetComponent<PlayerController>().activeWeapon == 1)
         {
-            ammunation.text = "∞";
-            totalAmmunation.text = "";
-            bar.text = "";
+            energy.sprite = topCapacity;
         }
         else
         {
-            ammunation.text = player.GetComponent<PlayerController>().ammunition.ToString();
-            totalAmmunation.text = totalAmmunationNum.ToString();
-            bar.text = "/";
+            Sprite spriteResult = null;
+            currentAmmunation = player.GetComponent<PlayerController>().ammunition;
+            if (currentAmmunation <= 100 && currentAmmunation > 75)
+            {
+                spriteResult = topCapacity;
+            }
+            else if (currentAmmunation <= 75 && currentAmmunation > 50)
+            {
+                spriteResult = topHalfCapacity;
+            }
+            else if (currentAmmunation <= 50 && currentAmmunation > 25)
+            {
+                spriteResult = halfCapacity;
+            }
+            else if (currentAmmunation <= 25 && currentAmmunation > 0)
+            {
+                spriteResult = lowHalfCapacity;
+            }
+            else if (currentAmmunation == 0)
+            {
+                spriteResult = lowCapacity;
+            }
+            energy.sprite = spriteResult;
         }
-        collectables.text = "Collected: " + player.GetComponent<PlayerController>().collectables;
+        collectables.text = player.GetComponent<PlayerController>().collectables.ToString();
     }
 }
