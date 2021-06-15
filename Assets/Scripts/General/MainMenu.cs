ce64d8331c8fade248e47583a02d8fa4c7df3333
DavidBarea <dbareag@edu.tecnocampus.cat>
2021-06-15 20:46:55 +0200

    Audio

--- Assets/Scripts/General/MainMenu.cs
+++ Assets/Scripts/General/MainMenu.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,4 +16,7 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    
+
 }
